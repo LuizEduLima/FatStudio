5d5478515798251d888f094d603fdfc9c2ecf081
Luiz Eduardo de Lima <luizedu_lima@hotmail.com>
2019-12-25 16:52:42 -0300

    Primeiro passo da criação da Classe de Contexto DbContext e DbSet para criação das tabela no banco de dados.

---
+++ FatStudio.Repositorio/Repositorios/Config/FatStudioContexto.cs
@@ -0,0 +1,22 @@
+﻿using FatStudio.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FatStudio.Repositorio.Repositorios.Config
+{
+    class FatStudioContexto:DbContext
+    {
+        
+        public DbSet<Atendimento>Atendimentos { get; set; }
+        public DbSet<Beneficiario> Beneficiarios { get; set; }
+        public DbSet<ItemAtendimento> ItemAtendimentos { get; set; }
+        public DbSet<Operadora> Operadoras { get; set; }
+        public DbSet<PlanoSaude> PlanoSaudes { get; set; }
+        public DbSet<Procedimento> Procedimentos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<ViaDeAcesso> ViaDeAcessos { get; set; }
+
+    }
+}
